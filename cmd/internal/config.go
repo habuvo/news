3626d08b11de9811bbaccecaadcd434293aedc42
habuvo <ilya@canto.ru>
2019-05-08 16:07:56 +0300

    add environment waiting

--- cmd/internal/config.go
+++ cmd/internal/config.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"github.com/habuvo/news/storage"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/nats-io/go-nats"
 	"log"
 	"os"
+	"time"
 )
 
 type Config struct {
@@ -15,29 +17,58 @@ type Config struct {
 	NewsStorage *storage.NewsDataStore
 }
 
+const tries = 20
+
 var Global *Config
 
-func GetConfig() (c *Config, err error) {
+func GetConfig() (con *Config, err error) {
 
-	c.DB, err = gorm.Open("postgres",
-		fmt.Sprintf("user=%s dbname=%s password=%s",
-			os.Getenv("POSTGRES_USER"),
-			os.Getenv("POSTGRES_DB"),
-			os.Getenv("POSTGRES_PASSWORD")))
-	if err != nil {
-		return
+	var c Config
+	//dirty hack for db and nats init waiting
+	tick := time.NewTicker(time.Second * 1)
+	defer tick.Stop()
+	tr := 1
+	for range tick.C {
+		c.NC, err = nats.Connect(nats.DefaultURL)
+		if err != nil {
+			if tr < tries {
+				tr++
+				continue
+			}
+			return nil, err
+		}
+		break
 	}
 
-	c.NC, err = nats.Connect(nats.DefaultURL)
-	if err != nil {
-		return
+	tr = 1
+	println(fmt.Sprintf("user=%s dbname=%s password=%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PASSWORD")))
+	for range tick.C {
+		c.DB, err = gorm.Open("postgres",
+			fmt.Sprintf("user=%s dbname=%s password=%s",
+				os.Getenv("POSTGRES_USER"),
+				os.Getenv("POSTGRES_DB"),
+				os.Getenv("POSTGRES_PASSWORD")))
+		if err != nil {
+			if tr < tries {
+				tr++
+				continue
+			}
+			return nil, err
+		}
+		break
 	}
 
 	c.NewsStorage = &storage.NewsDataStore{Conn: c.DB}
-	return
+	return &c, nil
 }
 
 func CloseConfig(c *Config) {
+	if c == nil || c.NC == nil || c.DB == nil {
+		return
+	}
 	err := c.NC.Drain()
 	if err != nil {
 		log.Printf("error drain nsq %v", err)
